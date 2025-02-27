a5243134197e112c3774f0d1a39a69b1d1ebcd13
swapnil2002-git <swapnilmudgade@gmail.com>
2025-02-27 13:06:54 +0530

    Delete BMVCProj10-MiniProject-DynamicSearch directory

--- BMVCProj10-MiniProject-DynamicSearch/src/main/java/com/sm/repository/IStudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sm.model.Student;
-
-public interface IStudentRepository extends JpaRepository<Student, Integer> {
-
-}
